919b4966cc0c621f6bcb033a162b553ba2aa84bb
amarantino159 <amarantino159@gmail.com>
2025-08-05 08:13:18 -0700

    vite init

---
+++ BookApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>here is my book review and details app!</h1>
+    </>
+  )
+}
+
+export default App
